045dadf2fb8d23577279e19dffc2dd5375b0c21c
Max Herrington <max.herrington@gmail.com>
2019-03-16 15:40:42 -0400

    first

---
+++ basic_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from basic_app.models import UserProfileInFo
+
+admin.site.register(UserProfileInFo)
---
+++ basic_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+
+class UserProfileInFo(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # additional classes
+    portfolio_site = models.URLField(blank=True)
+
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        return self.user.username
